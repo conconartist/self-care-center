4e2db52697340242e3655f1aaa88939363c04b68
Chung Hong <conconart.info@gmail.com>
2020-10-21 17:23:01 -0600

    Update HTML layout

---
+++ main.js
@@ -0,0 +1,10 @@
+<h1>Self Care Center</h1>
+    <section class="choose-message">
+      <p>What type of message?</p>
+      <input type="radio" label="affirmation"></input>
+      <input type="mantra" label="mantra"></input>
+      <button>Receive Message</button>
+    </section>
+    <section class="display-message">
+      <img src="" alt="">
+    </section>
